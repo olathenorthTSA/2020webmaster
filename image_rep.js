4a2e2601e35bf43ac8f36395199a17b0088b19b6
olathenorthTSA <49179193+olathenorthTSA@users.noreply.github.com>
2020-03-10 22:47:43 -0500

    Add files via upload

---
+++ image_rep.js
@@ -0,0 +1,15 @@
+function redBtn() {
+    document.getElementById("car").src="red.jfif"
+}
+
+function bluBtn() {
+    document.getElementById("car").src="blue.webp"
+}
+
+function silBtn() {
+    document.getElementById("car").src="silver.jpg"
+}
+
+function blkBtn() {
+    document.getElementById("car").src="black.jfif"
+}
