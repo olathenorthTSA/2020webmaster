457cb72a2fedc4e7449763c58a9717155196cf78
olathenorthTSA <49179193+olathenorthTSA@users.noreply.github.com>
2020-03-12 23:58:32 -0500

    Add files via upload

---
+++ image_rep.js
@@ -0,0 +1,50 @@
+// Sedan Buttons
+function redBtnSe() {
+    document.getElementById("car").src = "redSedan.png"
+}
+
+function bluBtnSe() {
+    document.getElementById("car").src = "blueSedan.png"
+}
+
+function silBtnSe() {
+    document.getElementById("car").src = "sedanExt.png"
+}
+
+function blkBtnSe() {
+    document.getElementById("car").src = "black.jfif"
+}
+
+//SUV Buttons
+function redBtnSv() {
+    document.getElementById("car").src = "redSUV.png"
+}
+
+function bluBtnSv() {
+    document.getElementById("car").src = "blueSUV.png"
+}
+
+function silBtnSv() {
+    document.getElementById("car").src = "suvExt.png"
+}
+
+function blkBtnSv() {
+    document.getElementById("car").src = "black.jfif"
+}
+
+//Truck Button
+function redBtnTk() {
+    document.getElementById("car").src = "redTruck.png"
+}
+
+function bluBtnTk() {
+    document.getElementById("car").src = "blueTruck.png"
+}
+
+function silBtnTk() {
+    document.getElementById("car").src = "truckExt.png"
+}
+
+function blkBtnTk() {
+    document.getElementById("car").src = "black.jfif"
+}
